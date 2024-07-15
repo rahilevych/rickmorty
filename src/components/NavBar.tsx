ce4bb6f61491b4dcd2a38e416e95a06bbcd77e0e
AlonaRahilevych <ragilevic@gmail.com>
2024-07-15 14:07:19 +0200

    NavBar was changed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,25 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 function NavBar() {
+  const path = usePathname();
+  console.log(path);
   return (
-    <nav>
-      <ul className=''>
-        <li></li>
-        <li></li>
-        <li></li>
+    <nav className=' bg-indigo-900 h-20'>
+      <ul className='flex flex-row justify-evenly items-center h-full'>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          {' '}
+          <Link href='/characters'>Characters</Link>
+        </li>
+        <li>
+          {' '}
+          <Link href='/static'>Static</Link>
+        </li>
       </ul>
     </nav>
   );
